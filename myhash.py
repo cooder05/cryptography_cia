5b8114709501d14cd5f5502484e86efa0cca23d5
cooder05 <97331477+cooder05@users.noreply.github.com>
2026-04-02 08:51:22 +0530

    Update myhash.py

--- myhash.py
+++ myhash.py
@@ -1,11 +1,6 @@
-def clean_text(s):
-    return ''.join([c for c in s.upper() if c in string.ascii_uppercase])
-def pad_text(s, n):
-    while len(s) % n != 0:
-        s += 'X'
-    return s
+from hill import clean_text,pad_text
 
-def text_2_binblocks(text):
+def atext_2_binblocks(text):
     blocks = []
     for i in range(0,len(text),2):
         bin1 = str(format(ord(text[i]), '08b'))     #8-bit binary
@@ -15,9 +10,9 @@ def text_2_binblocks(text):
     return blocks
     
 
-def xor16_hash(htext):
+def xor16_hash(htext,k):
     msg = clean_text(htext)
-    msg += "GYBNQKURP"   #key in array form
+    msg += k   #key in array form
     msg = pad_text(msg,2)
     blocks = text_2_binblocks(msg)
     result = 0
@@ -26,4 +21,4 @@ def xor16_hash(htext):
         result ^= int(b, 2)   # convert binary string -> int and XOR
 
     xor_binary = format(result, '016b')
-    print("hash: ",xor_binary)
+    return xor_binary
