8a6812aade4f9137a43c677d0eac04cc1a87f7a3
cooder05 <97331477+cooder05@users.noreply.github.com>
2026-04-02 08:50:24 +0530

    Update hill.py

--- hill.py
+++ hill.py
@@ -64,34 +64,3 @@ def hill_decrypt(ciphertext):
         plaintext += vec_to_text(decrypted)
 
     return plaintext
-
-encrypt = hill_encrypt(text)
-print("cyphertext: ",encrypt)
-fin = hill_decrypt(encrypt,)
-print("plaintext: ",fin)
-
-def text_2_binblocks(text):
-    blocks = []
-    for i in range(0,len(text),2):
-        bin1 = str(format(ord(text[i]), '08b'))     #8-bit binary
-        bin2 = str(format(ord(text[i+1]),'08b'))    #8-bit binary
-        block = bin1+bin2                           #16-bit binary
-        blocks.append(block)
-    return blocks
-    
-
-
-def myhash(htext):
-    msg = clean_text(htext)
-    msg += "GYBNQKURP"   #key in array form
-    msg = pad_text(msg,2)
-    blocks = text_2_binblocks(msg)
-    result = 0
-
-    for b in blocks:
-        result ^= int(b, 2)   # convert binary string -> int and XOR
-
-    xor_binary = format(result, '016b')
-    print("hash: ",xor_binary)
-
-myhash(text)
